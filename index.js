094f2f11c0ae36192399019c05b11b1f62d286f1
zulham.sy@gmail.com <zulham.sy@gmail.com>
2022-06-03 09:06:14 +0700

    delete default value parameter

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sortAndStringfy(list) {
 	return result.join('-')
 }
 
-const createDate = function(dates, index = null) {
+const createDate = function(dates, index) {
 	// execute
 	const result = []
 	const sortResult = []
