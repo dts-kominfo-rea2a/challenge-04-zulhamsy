6d5cf6b225c2fa8f01253cd17d3c54d74a71bf54
zulham.sy@gmail.com <zulham.sy@gmail.com>
2022-06-03 08:53:41 +0700

    first test

--- index.js
+++ index.js
@@ -8,20 +8,55 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+function sortAndStringfy(list) {
+	const initial = list
+	const result = []
+	initial.sort((a, b) => a - b)
+	for (let i = 0; i < initial.length; i++) {
+		result.push(initial[i].toString())
+	}
+	
+	return result
+}
+
+const createDate = function(dates, index = null) {
+	// check first argument
+	if (!Array.isArray(dates)) {
+		console.warn('Invalid first argument, you should pass Array of String')
+		return
+	}
+	// check second argument
+	if (!Number.isInteger(index) && index !== null) {
+		console.warn('Invalid second argument, you should pass Number between 0 and ' + dates.length - 1 + ' instead of ' + typeof index)
+		console.info('Or you can leave it blank')
+		return
+	}
+	// execute
+	const result = []
+	const sortResult = []
+	for (let i = 0; i < dates.length; i++) {
+		result.push(Date.parse(dates[i]) ? Date.parse(dates[i]) / 1000 : 'Invalid date format')
+	}
+	// output
+	if (index < dates.length && index > -1) {
+		return result[index].toString()
+	} else {
+		return sortAndStringfy(result)
+	}
+}
 
 // ! JANGAN DIMODIFIKASI
 (() => {
-  // IIFE
+	// IIFE
 
-  // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
-  console.log(createDate?.(dates));
+	// '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
+	console.log(createDate?.(dates));
 
-  // '1614841200' (dalam string)
-  console.log(createDate?.(dates, 2));
+	// '1614841200' (dalam string)
+	console.log(createDate?.(dates, 2));
 })();
 
 module.exports = {
-  dates,
-  createDate,
+	dates,
+	createDate,
 };
